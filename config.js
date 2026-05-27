b211478fa9f2c4285e57e9879088e8e27b7b15b1
DeWet-Fourie <dewetfourie08@gmail.com>
2026-05-27 20:46:57 +0200

    Delete config.js

--- config.js
+++
@@ -1,9 +0,0 @@
-window.BAADJIE_CONFIG = {
-  // Paste your published Google Sheets CSV link here.
-  SHEET_CSV_URL: "",
-
-  // Format: South African WhatsApp number without +, spaces, or brackets.
-  WHATSAPP_NUMBER: "27000000000",
-
-  BRAND_EMAIL: "hello@baadjie.co.za"
-};
