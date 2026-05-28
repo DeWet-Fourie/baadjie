10b4d36631c1679e7bf79885e3afb3e0b56907af
DeWet-Fourie <dewetfourie08@gmail.com>
2026-05-28 22:16:45 +0200

    Update config.js

--- config.js
+++ config.js
@@ -1,9 +1,9 @@
-window.BAADJIE_CONFIG = {
-  // Paste your published Google Sheets CSV link here.
-  https://docs.google.com/spreadsheets/d/e/2PACX-1vSKmI-Y-b_a4v8VbeJrNpAQjdsMoZfxKXCCLbvW92GhLzTlJQxQjS8htJ-W5df4n-cSO_jY3Yh_QgUD/pub?gid=0&single=true&output=csv: "",
+const CONFIG = window.BAADJIE_CONFIG || {};
 
-  // Format: South African WhatsApp number without +, spaces, or brackets.
-  WHATSAPP_NUMBER: "27000000000",
+const SHEET_CSV_URL =
+  "https://docs.google.com/spreadsheets/d/e/2PACX-1vSKmI-Y-b_a4v8VbeJrNpAQjdsMoZfxKXCCLbvW92GhLzTlJQxQjS8htJ-W5df4n-cSO_jY3Yh_QgUD/pub?gid=0&single=true&output=csv";
+
+const WHATSAPP_NUMBER = CONFIG.WHATSAPP_NUMBER || "27000000000";
 
   BRAND_EMAIL: "hello@baadjie.co.za"
 };
