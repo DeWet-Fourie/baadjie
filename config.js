112ab47964118fbe92dd934d248397efa51411b0
DeWet-Fourie <dewetfourie08@gmail.com>
2026-05-28 22:12:42 +0200

    Update config.js

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 window.BAADJIE_CONFIG = {
   // Paste your published Google Sheets CSV link here.
-  SHEET_CSV_URL: "",
+  https://docs.google.com/spreadsheets/d/e/2PACX-1vSKmI-Y-b_a4v8VbeJrNpAQjdsMoZfxKXCCLbvW92GhLzTlJQxQjS8htJ-W5df4n-cSO_jY3Yh_QgUD/pub?gid=0&single=true&output=csv: "",
 
   // Format: South African WhatsApp number without +, spaces, or brackets.
   WHATSAPP_NUMBER: "27000000000",
