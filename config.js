0e469b746e8b79f5fc86c7ccb878fb6da6a64bf7
DeWet-Fourie <dewetfourie08@gmail.com>
2026-05-28 22:45:52 +0200

    Delete config.js

--- config.js
+++
@@ -1,9 +0,0 @@
-const CONFIG = window.BAADJIE_CONFIG || {};
-
-const SHEET_CSV_URL =
-  "https://docs.google.com/spreadsheets/d/e/2PACX-1vSKmI-Y-b_a4v8VbeJrNpAQjdsMoZfxKXCCLbvW92GhLzTlJQxQjS8htJ-W5df4n-cSO_jY3Yh_QgUD/pub?gid=0&single=true&output=csv";
-
-const WHATSAPP_NUMBER = CONFIG.WHATSAPP_NUMBER || "27000000000";
-
-  BRAND_EMAIL: "hello@baadjie.co.za"
-};
